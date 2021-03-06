ba0913132a4dc38beea51186c2893aa1b3788c00
Đinh Văn Đức <ducdv.bk94@gmail.com>
2021-03-06 10:28:17 +0700

    The shop app project

---
+++ store/actions/products.js
@@ -0,0 +1,132 @@
+import Product from "../../models/product";
+
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://fir-demo-e7bb2-default-rtdb.firebaseio.com/products.json"
+      );
+      if (!response.ok) {
+        const resData = await response.resData;
+        console.log(resData);
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts,
+        userProducts: loadedProducts.filter((prod) => prod.ownerId === "u1"),
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
+
+export const createProduct = (title, description, imageUrl, price) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://fir-demo-e7bb2-default-rtdb.firebaseio.com/products.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    const resData = await response.json();
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+      },
+    });
+  };
+};
+
+export const updateProduct = (id, title, description, imageUrl) => {
+  return async (dispatch, getState) => {
+    console.log(getState());
+    const response = await fetch(
+      `https://fir-demo-e7bb2-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      console.log("Something went wrong");
+      throw new Error("Something went wrong!");
+    }
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+      },
+    });
+  };
+};
+
+export const deleteProduct = (productId) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `https://fir-demo-e7bb2-default-rtdb.firebaseio.com/products/${id}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        console.log("Something went wrong!");
+        throw new Error("Something went wrong!");
+      }
+      dispatch({ type: DELETE_PRODUCT, pid: productId });
+    } catch (err) {
+      console.log(err.message);
+      throw err;
+    }
+  };
+};
---
+++ store/reducers/products.js
@@ -0,0 +1,71 @@
+import Product from "../../models/product";
+import {
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+  SET_PRODUCTS,
+} from "../actions/products";
+
+const initialState = {
+  availableProducts: [],
+  userProducts: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return {
+        availableProducts: action.products,
+        userProducts: action.userProducts,
+      };
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter(
+          (product) => product.id !== action.pid
+        ),
+        availableProducts: state.availableProducts.filter(
+          (product) => product.id !== action.pid
+        ),
+      };
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        action.productData.id,
+        "u1",
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        action.productData.price
+      );
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+    case UPDATE_PRODUCT:
+      const productIndex = state.userProducts.findIndex(
+        (product) => product.id === action.pid
+      );
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[productIndex].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIndex].price
+      );
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIndex] = updatedProduct;
+      const pId = state.availableProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedUserProducts[pId] = updatedProduct;
+      return {
+        userProducts: updatedUserProducts,
+        availableProducts: updatedAvailableProducts,
+      };
+    default:
+      return state;
+  }
+};
